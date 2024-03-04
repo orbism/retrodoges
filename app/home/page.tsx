d7cb904e5e66b19d0978c61146c5e83ac1d54ff4
orbgasm <orb@orbatron.org>
2024-03-04 08:04:09 -0500

    postmint cleanup homepage ticker

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -23,58 +23,6 @@ const images = [
     return (
     
       <div className="homepage">
-
-          <div className="position-relative marquee-container d-none d-sm-block">
-          <a href="https://retrodoges.com/mint">.</a>
-            
-
-            <div className="marquee d-flex justify-content-around">
-
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-
-
-            </div>
-            <div className="marquee marquee2 d-flex justify-content-around">
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-              <span>MINTING NOW</span>
-            </div>
-          </div>
         
         <div className="infomatic">
           <p>
